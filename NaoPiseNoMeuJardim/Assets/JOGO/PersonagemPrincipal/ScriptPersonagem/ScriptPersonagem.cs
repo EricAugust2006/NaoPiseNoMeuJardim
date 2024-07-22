9e19b03228112b457b491339b29de3963bc91460
Eric Augusto <ericaugust2006@gmail.com>
2024-07-22 16:59:50 -0300

    MovimentacaoPersonagem

---
+++ NaoPiseNoMeuJardim/Assets/JOGO/PersonagemPrincipal/ScriptPersonagem/ScriptPersonagem.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class ScriptPersonagem : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    [SerializeField] private float speed = 3f;
+    private Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        float VelX = Input.GetAxis("Horizontal");
+
+        Vector3 Movement = new Vector3(VelX, 0f, 0f);
+        transform.position += Movement * Time.deltaTime * speed;
+
+        if(VelX != 0)
+        {
+            animator.SetBool("Correndo", true);
+        }
+        else
+        {
+            animator.SetBool("Correndo", false);
+        }
+    }
+}
