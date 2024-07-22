f08fb0c448e09bd91f2ac88bea69c5104df326d7
Eric Augusto <ericaugust2006@gmail.com>
2024-07-22 17:18:59 -0300

    Detalhes

--- NaoPiseNoMeuJardim/Assets/JOGO/PersonagemPrincipal/ScriptPersonagem/ScriptPersonagem.cs
+++ NaoPiseNoMeuJardim/Assets/JOGO/PersonagemPrincipal/ScriptPersonagem/ScriptPersonagem.cs
@@ -9,11 +9,13 @@ public class ScriptPersonagem : MonoBehaviour
     private Rigidbody2D rb;
     [SerializeField] private float speed = 3f;
     private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
@@ -27,6 +29,16 @@ public class ScriptPersonagem : MonoBehaviour
 
         Vector3 Movement = new Vector3(VelX, 0f, 0f);
         transform.position += Movement * Time.deltaTime * speed;
+
+        if(VelX >= 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else
+        {
+            spriteRenderer.flipX = true;
+        }
+
 
         if(VelX != 0)
         {
