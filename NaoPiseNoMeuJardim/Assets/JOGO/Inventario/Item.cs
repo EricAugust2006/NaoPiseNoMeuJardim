ea609bd37bc954b2f0f439f3c5fa2e3ee0906be1
Eric Augusto <ericaugust2006@gmail.com>
2024-08-22 17:11:19 -0300

    retirando Inventario

---
+++ NaoPiseNoMeuJardim/Assets/JOGO/Inventario/Item.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+   public string nome;
+   public string descricao;
+   //public Sprite icone;
+   public bool coletado = false;
+}
